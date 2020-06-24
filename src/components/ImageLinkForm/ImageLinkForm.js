a3e3a3b0b62ec4a3a15005002e4ad83ba7752884
Ruben Plaza <ruben.d.plaza@ryerson.ca>
2020-06-24 15:36:42 -0400

    init commit. main page setup and api connection

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = ({ onInputChange, onButtonSubmit, onKeyPressSubmit }) => {
+    return (
+        <div>
+            <p className='f3'>
+                {'This Magic Brain will detect faces in your pictures. Give it a try.'}
+            </p>
+            <div className='center'>
+                <div className='form center pa4 br3 shadow-5'>
+                    <input type="text" onChange={onInputChange}onKeyPress={onKeyPressSubmit}className='f4 pa2 w-70 center'></input>
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-blue' onClick={onButtonSubmit}
+                    >Detect</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ImageLinkForm;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navigation.css';
+
+const Navigation = () => {
+    return (
+        <nav className='nav-bar'>
+            <p className='f3 link dim black pa3 pointer'>Sign Out</p>
+        </nav>
+    );
+}
+
+export default Navigation;
