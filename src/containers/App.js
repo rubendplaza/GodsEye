11b93a109fbc1a02eba3ca03a4a20c59151dfd6f
Ruben Plaza <ruben.d.plaza@ryerson.ca>
2020-07-01 01:09:04 -0400

    bug in fetch for image route url to server

--- src/containers/App.js
+++ src/containers/App.js
@@ -94,7 +94,7 @@ class App extends React.Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('https://limitless-fjord-79432.herokuapp.com:3000/image', {
+        fetch('https://limitless-fjord-79432.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
